4395ed5b43b6a04631083b8c438ca7b9b6af65db
adamarispadilla <72954890+adamarispadilla@users.noreply.github.com>
2023-09-07 04:49:25 -0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
-const squares = document.querySelectorAll('.square');
+  const squares = document.querySelectorAll('.square');
 
-squares.forEach((square) => {
-  square.addEventListener('click', () => {
-    square.classList.toggle('flipped');
+  squares.forEach((square) => {
+    square.addEventListener('click', () => {
+      square.classList.toggle('flipped');
+    });
   });
-});
