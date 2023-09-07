466b8d3fe6d82c6f904d62ec919d58744e613b66
adamarispadilla <72954890+adamarispadilla@users.noreply.github.com>
2023-09-07 00:55:47 -0700

    tooltip

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,18 @@
-
 const tooltipTriggers = document.querySelectorAll('.tooltip-trigger');
 
-// Iterate through each tooltip trigger element
 tooltipTriggers.forEach(trigger => {
-  // Get the tooltip text from the "data-tooltip" attribute
   const tooltipText = trigger.getAttribute('data-tooltip');
 
-  // Create a tooltip element
   const tooltip = document.createElement('div');
   tooltip.className = 'tooltip';
   tooltip.textContent = tooltipText;
 
-  // Append the tooltip to the body
   document.body.appendChild(tooltip);
 
-  // Show the tooltip when hovering over the trigger
   trigger.addEventListener('mouseover', () => {
     tooltip.style.opacity = '1';
   });
 
-  // Hide the tooltip when the mouse leaves the trigger
   trigger.addEventListener('mouseout', () => {
     tooltip.style.opacity = '0';
   });
