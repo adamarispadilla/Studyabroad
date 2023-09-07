19a52b9478181e364fa8108cc3bb9a368d67fca7
adamarispadilla <72954890+adamarispadilla@users.noreply.github.com>
2023-09-07 00:51:35 -0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,26 @@
-/* 
-most jquery js documents start
-with the $(document).ready function.
-This ensures the document is
-ready for the code inside of the
-function to be executed 
-*/
 
-$(document).ready(function() {
-    
-    /* 
-    jquery follows its own syntax. it uses
-    the $ symbol to look for an element. 
-    followed by () which is the element to find
-    inside of the parentheses. followed a
-    . and an event. this event then triggers the
-    following function, executing the code
-    inside the curly brackets.
-    */
+const tooltipTriggers = document.querySelectorAll('.tooltip-trigger');
 
-    $('body').click(function(){
-        $(this).css('background', 'yellow');
-    })
+// Iterate through each tooltip trigger element
+tooltipTriggers.forEach(trigger => {
+  // Get the tooltip text from the "data-tooltip" attribute
+  const tooltipText = trigger.getAttribute('data-tooltip');
 
-    /* random background color from an array */
+  // Create a tooltip element
+  const tooltip = document.createElement('div');
+  tooltip.className = 'tooltip';
+  tooltip.textContent = tooltipText;
 
-    // let colors = ['blue', 'red', 'green', 'purple', 'yellow']
-    // $('body').click(function(){
-    //     var color = colors[Math.floor(Math.random() * colors.length)];
-    //     $(this).css('background', color);
-    // });
+  // Append the tooltip to the body
+  document.body.appendChild(tooltip);
 
-    /* add a character to a list */
+  // Show the tooltip when hovering over the trigger
+  trigger.addEventListener('mouseover', () => {
+    tooltip.style.opacity = '1';
+  });
 
-    // $('li').mouseover(function(){
-    //     $(this).append('👋');
-    // });
-
-    /* make a bug follow your cursor and resize */
-
-    // $('body').append('<div id="follow">🦟</div>');
-    // $(document).on("mousemove", function (event) {
-    //     $('#follow').css({
-    //         'font-size' : event.pageY/10+'px',
-    //         'position' : 'absolute',
-    //         'left' : event.pageX+10+'px',
-    //         'top' : event.pageY+10+'px'
-    //     });
-    // });
-
-
+  // Hide the tooltip when the mouse leaves the trigger
+  trigger.addEventListener('mouseout', () => {
+    tooltip.style.opacity = '0';
+  });
 });
