9bf68949d65afe9b2c88db76afac982e493b05f6
adamarispadilla <72954890+adamarispadilla@users.noreply.github.com>
2023-09-07 05:17:32 -0700

    updates

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const squareContainer = document.querySelector('.square-container');
 
   squareContainer.addEventListener('click', (event) => {
-    const clickedSquare = event.target.closest('.square');
+    const clickedSquare = event.target.closest('.face front');
     if (clickedSquare) {
       clickedSquare.classList.toggle('flipped');
     }
